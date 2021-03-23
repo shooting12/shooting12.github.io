ce07891b8f68399ac29bc18d491e254f1ff01e91
Shooting Chuang <shooting12@users.noreply.github.com>
2021-03-24 00:33:03 +0800

    [ToFish] Add showing dating photo when click on certain date.
    * Add event when click on date, showing the related photo.
    * User can choose whether to show different photo when click on date.
       - Revise SHOW_IMAGE_AND_CAN_JUMP: true (dynamic change photo).
    * Need to fix some bug.
      - Photo seems flash when click new date.
      - Jump to photo cannot fix to a good position, it varies.

--- ToFish/script/main.js
+++ ToFish/script/main.js
@@ -1,4 +1,8 @@
+// Whether to turn on showing image
+var SHOW_IMAGE_AND_CAN_JUMP = true;
+// Build go out event dictionary
 var eventDict = createEventDictionary();
+
 
 function myFunction(year, month) {
   /* Change title of calendar */
@@ -27,13 +31,16 @@ function myFunction(year, month) {
 
   var eventDays = getEventDayInMonth(eventDict, year, month)
   console.log(eventDays)
-  html += generateDaysHTML(dayInMonthList, eventDays)
 
-  
+  if (SHOW_IMAGE_AND_CAN_JUMP) {
+    html += generateDaysLinkHTML(dayInMonthList, eventDays, year, month)
+  } else {
+    html += generateDaysHTML(dayInMonthList, eventDays)
+    
+  }
 
   // Display the calendar by assigning html
   document.getElementById("dynamic_calendar_number").innerHTML = html;
-
 }
 
 function getDayInMonthList(year, month) {
@@ -116,9 +123,32 @@ function generateDaysHTML(dayInMonthList, eventDays) {
       else if (eventDays.includes(dayInMonthList[i])) {
         // Create special tag to indicate this is a special day 
         // by checking whether this day is in the event list.
-        html += '<div class="calendar__number calendar__number--event">' + dayInMonthList[i] + '</div>'
+        html += '<div class="calendar__number calendar__number--event">' + dayInMonthList[i] + '</div>';
       } else {
-        html += '<div class="calendar__number">' + dayInMonthList[i] + '</div>'
+        html += '<div class="calendar__number">' + dayInMonthList[i] + '</div>';
+      }
+    }
+
+    return html;
+}
+
+function generateDaysLinkHTML(dayInMonthList, eventDays, year, month) {
+    var html = "";
+
+    for (var i = 0; i < dayInMonthList.length; i++) {
+      if (dayInMonthList[i] == 0) {
+        // Empty space in the beginning of month
+        html += '<div class="calendar__number"></div>'
+      }
+      else if (eventDays.includes(dayInMonthList[i])) {
+        /* Create special tag to indicate this is a special day 
+           by checking whether this day is in the event list. */
+
+        // Generate id name for onClick event.
+        var id_name = "click_" + getPictureFileIndex(year, month, dayInMonthList[i]);
+        html += '<div class="calendar__number calendar__number--event" id="' + id_name + '" onClick="onClickToImage(this.id)">' + dayInMonthList[i] + '</div>';        
+      } else {
+        html += '<div class="calendar__number">' + dayInMonthList[i] + '</div>';
       }
     }
     
@@ -128,27 +158,52 @@ function generateDaysHTML(dayInMonthList, eventDays) {
 function createEventDictionary() {
     var dict = {};
     dict['201912'] = [24];
-    dict['20201'] = [19];
-    dict['20205'] = [17, 24, 31];
-    dict['20206'] = [5, 10, 14, 25, 26, 27, 28];
-    dict['20207'] = [5, 11];
-    dict['20208'] = [8, 12, 15, 22, 23];
+    dict['202001'] = [19];
+    dict['202005'] = [17, 24, 31];
+    dict['202006'] = [5, 10, 14, 25, 26, 27, 28];
+    dict['202007'] = [5, 11];
+    dict['202008'] = [8, 12, 15, 22, 23];
     dict['202010'] = [25];
     dict['202011'] = [26];
     dict['202012'] = [5, 9, 13, 21, 26, 31];
-    dict['20211'] = [1, 4, 7, 11, 15, 21, 24, 28];
-    dict['20212'] = [7];
+    dict['202101'] = [1, 4, 7, 11, 15, 21, 24, 28];
+    dict['202102'] = [7];
 
     console.log(dict);
-    var year = 2020;
-    var month = 5;
-    var monthStr = "" + year + month;
-    console.log(monthStr);
-    console.log(dict[monthStr]);
     return dict;
 }
 
 function getEventDayInMonth(eventDict, year, month) {
-    var monthStr = "" + year + month;
+    var formattedMonth = ("0" + month).slice(-2); // format 5 to 05
+    var monthStr = "" + year + formattedMonth;
     return eventDict[monthStr];
+}
+
+function getPictureFileIndex(year, month, day) {
+    /* Given 2020 and 5, then get 2005 */
+    formattedYear = ("0" + year).slice(-2)
+    formattedMonth = ("0" + month).slice(-2);
+    formattedDay = ("0" + day).slice(-2);
+    picture_name = "" + formattedYear + formattedMonth + formattedDay;
+    return picture_name;
+}
+
+function onClickToImage(clicked_id) {
+    //alert(clicked_id);
+
+    // Jump to show image
+    window.location.href="#date_picture";
+    //window.location.hash = '#date_picture';
+    // Dynamically give image source
+    var picture_name = clicked_id.slice(-6) + '.JPG';
+    //alert(picture_name);
+    document.getElementById("date_picture").src = "img\\DatePicture\\" + picture_name;
+    //document["date_picture"].src = ;
+
+    /* need to stop the form sending of the form
+     UPDATE as comment: This may not be exactly correct syntax 
+     for stopping a form , look up preventing form submission  */
+    
+    //e.preventDefault();
+    //e.stopPropagation(); 
 }
